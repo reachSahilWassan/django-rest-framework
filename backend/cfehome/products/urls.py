26c6b9c37b0c8a6ee3735d52a9a13a185731122c
Sahil Wassan <sahil.wassan@avasant.com>
2023-09-14 22:27:13 +0530

    tried adding and viewing all products data through api_view

--- backend/cfehome/products/urls.py
+++ backend/cfehome/products/urls.py
@@ -3,7 +3,12 @@ from . import views
 
 urlpatterns = [
     path("", views.ProductCreateAPIView.as_view()),
+    # path("", views.product_alt_view),
+
+    # path("<int:pk>/", views.product_alt_view),
     path("<int:pk>/", views.ProductDetailAPIView.as_view()),
+
     path("list-view", views.ProductListAPIView.as_view()),
     path("list-and-create", views.ProductListCreateAPIView.as_view()),
+    # path("add-or-get-data", views.product_alt_view),
 ]
--- backend/cfehome/products/views.py
+++ backend/cfehome/products/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from products.serializers import ProductSerializer
 from rest_framework import generics
 from products.models import Products
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
 
@@ -32,3 +36,24 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save()
 
+
+
+@api_view(["GET", "POST"])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+    if method == "GET":
+        if pk is not None:
+            obj = get_object_or_404(Products, pk = pk)
+            data = ProductSerializer(obj, many = False).data
+            return Response(data)
+
+        queryset = Products.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+
+    if method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+    
