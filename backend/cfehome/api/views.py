fcb5f73a6b652a79abf95a6da37f32dfb1624003
Sahil Wassan <sahil.wassan@avasant.com>
2023-09-14 12:55:12 +0530

    added serializers.py and modified the views.py of products

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -5,6 +5,11 @@ import logging
 from pdb import set_trace
 from products.models import Products
 from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.serializers import ModelSerializer
+
+from products.serializers import ProductSerializer
 
 
 logging.basicConfig(level=logging.INFO)
@@ -12,13 +17,15 @@ logging.basicConfig(level=logging.INFO)
 # Create your views here.
 
 
+@api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs) -> dict:
+    """DRF API View"""
+
     product_data = Products.objects.all().order_by("?").first()
     data = {}
-    try:
-        if product_data:
-            data = model_to_dict(product_data)
-    except:
-        logging.exception("ERROR")
+
+    if product_data:
+        # data = model_to_dict(product_data)
+        data = ProductSerializer(product_data).data
     # data["headers"] = request.headers
-    return JsonResponse(data)
+    return Response(data)
